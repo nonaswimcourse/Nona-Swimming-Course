3125f3765504789788c8916156bd319d1047be7e
nona swimming course <keishaayundiainara27@gmail.com>
2026-06-15 04:44:55 +0700

    Update Supabase URL to include REST endpoint

--- absensi/script.js
+++ absensi/script.js
@@ -1,7 +1,7 @@
 const TOTAL_PERTEMUAN = 12; // Total target pertemuan les
 
 // Inisialisasi Supabase Client (PASTIKAN AMBIL KODE BARU INI)
-const SUPABASE_URL = "https://mjfwgmhuengvfdagbcsk.supabase.co";
+const SUPABASE_URL = "https://mjfwgmhuengvfdagbcsk.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qZndgmWh1ZW5ndmZkYWdiY3NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMDczMTMsImV4cCI6MjA5Njg4MzMxM30.NxZY9zHP9zQmHRsgpcGZyk3t7_xaGFFuTa3bYIAD384";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
